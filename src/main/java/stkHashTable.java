9252d4419b30d71a6553005366a1a66d3bd5ab1d
vasavi vempati <vasavivempati2@gmail.com>
2019-01-10 16:24:22 -0800

    added code

--- src/main/java/stkHashTable.java
+++ src/main/java/stkHashTable.java
@@ -5,14 +5,30 @@ import java.util.Hashtable;
  */
 public class stkHashTable {
     public void process_input(String[] input){
-        Hashtable<Integer,String> output = new Hashtable<Integer, String>();
-        for(int i =0; i<input.length;i++){
+        Hashtable<Integer,Integer> output = new Hashtable<Integer, Integer>();
+        for(int i =1; i<input.length;i++){
             if(input[i].contains("push")){
+                String[] number = input[i].split(" ");
+                output.put(i,Integer.valueOf(number[1]));
+            }
+            else if(input[i].contains("pop") && (output.isEmpty() == false)){
+                //int index = output.size() -1;
+                output.remove(output.size());
 
             }
-            else if(input[i].contains("pop")){
-
+            else if(input[i].contains("inc")){
+                String[] number = input[i].split(" ");
+                int add_on = Integer.valueOf(number[2]);
+                int number_of_values_to_change =  Integer.valueOf(number[2]);
+                for(int j=output.size()-1;j>output.size()-number_of_values_to_change;j--){
+                    int val = output.get(j);
+                    output.put(j,val+add_on);
+                }
             }
+            for (Integer key : output.keySet()) {
+                System.out.print(output.get(key) + " ");
+            }
+            System.out.println();
         }
     }
 }
